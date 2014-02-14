b811fcc8313127c81c1540f76865ad10115cb501
dbrezoev <d.brezoev@gmail.com>
2014-02-14 07:19:54 -0800

    barking dog

---
+++ OOP/OOPPrinciplesPart1/TryHere/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Media;
+class Dog
+{
+    public string Name { get; private set; }
+    public int Age { get; set; }
+
+    public Dog(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public void Bark()
+    {
+        var sound = new SoundPlayer(@"bark.wav");        
+        sound.PlaySync();        
+    }
+}
+class Program
+{    
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog("pesho", 5);
+        dog.Bark();
+    }
+}
