56c75d1a88b285eaac567091e2ea84860267127b
dbrezoev <d.brezoev@gmail.com>
2014-06-01 00:04:50 +0300

    CodeTuning

---
+++ QualityCode/CodeTuning/02.Comaprison/Program.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _02.Comparison
+{
+    public class Comparer
+    {
+        public const long COUNTER = 100000000;
+        public static readonly Stopwatch st = new Stopwatch();
+        static void Main(string[] args)
+        {
+            GetResult("Add integers", () =>
+            {
+                int result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result += i;
+                }
+            });
+
+            GetResult("Add Longs", () =>
+            {
+                long result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result += i;
+                }
+            });
+
+            GetResult("Add Doubles", () =>
+            {
+                double result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result += i;
+                }
+            });
+
+            GetResult("Add Floats", () =>
+            {
+                float result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result += i;
+                }
+            });
+
+            GetResult("Add Decimals", () =>
+            {
+                decimal result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result += i;
+                }
+            });
+
+            Console.WriteLine(new string('-', Console.WindowWidth));
+
+            GetResult("Subtract Ints", () =>
+            {
+                int result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result--;
+                }
+            });
+
+            GetResult("Subtract Longs", () =>
+            {
+                long result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result--;
+                }
+            });
+
+            GetResult("Subtract Doubles", () =>
+            {
+                double result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result--;
+                }
+            });
+
+            GetResult("Subtract Floats", () =>
+            {
+                float result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result--;
+                }
+            });
+
+            GetResult("Subtract Decimals", () =>
+            {
+                decimal result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result--;
+                }
+            });
+
+            Console.WriteLine(new string('-', Console.WindowWidth));
+
+            GetResult("Increment Ints", () =>
+            {
+                int result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result++;
+                }
+            });
+
+            GetResult("Increment Longs", () =>
+            {
+                long result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result++;
+                }
+            });
+
+            GetResult("Increment Doubles", () =>
+            {
+                double result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result++;
+                }
+            });
+
+            GetResult("Increment Floats", () =>
+            {
+                float result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result++;
+                }
+            });
+
+            GetResult("Increment Decimals", () =>
+            {
+                decimal result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result++;
+                }
+            });
+
+            Console.WriteLine(new string('-', Console.WindowWidth));
+
+            GetResult("Multiply Ints", () =>
+            {
+                int result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result *= 1;
+                }
+            });
+
+            GetResult("Multiply Longs", () =>
+            {
+                long result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result *= 1;
+                }
+            });
+
+
+            GetResult("Multiply Doubles", () =>
+            {
+                double result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result *= 1;
+                }
+            });
+
+            GetResult("Multiply Floats", () =>
+            {
+                float result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result *= 1;
+                }
+            });
+
+            GetResult("Multiply Decimals", () =>
+            {
+                decimal result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result *= 1;
+                }
+            });
+
+            Console.WriteLine(new string('-', Console.WindowWidth));
+
+            GetResult("Divide Ints", () =>
+            {
+                int result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result /= 1;
+                }
+            });
+
+            GetResult("Multiply Longs", () =>
+            {
+                long result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result *= 1;
+                }
+            });
+
+            GetResult("Multiply Doubles", () =>
+            {
+                double result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result *= 1;
+                }
+            });
+
+            GetResult("Multiply Floats", () =>
+            {
+                float result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result *= 1;
+                }
+            });
+
+            GetResult("Multiply Decimals", () =>
+            {
+                decimal result = 0;
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    result *= 1;
+                }
+            });
+        }
+
+        public static void GetResult(string title, Action action)
+        {
+            Console.WriteLine(title);
+            st.Restart();
+            action();
+            st.Stop();
+            Console.WriteLine(st.Elapsed);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ QualityCode/CodeTuning/03.CompareFunctions/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _02.Comparison
+{
+    public class Comparer
+    {
+        public const long COUNTER = 100000;
+        public static readonly Stopwatch st = new Stopwatch();
+        static void Main(string[] args)
+        {
+            GetResult("Square root float", () =>
+            {
+                for (float i = 0; i < COUNTER; i++)
+                {
+                    Math.Sqrt(i);
+                }
+            });
+
+            GetResult("Square root double", () =>
+            {
+                for (double i = 0; i < COUNTER; i++)
+                {
+                    Math.Sqrt(i);
+                }
+            });
+
+            GetResult("Square root int", () =>
+            {
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    Math.Sqrt(i);
+                }
+            });
+
+            Console.WriteLine(new string('-', Console.WindowWidth));
+
+            GetResult("Log double", () =>
+            {
+                for (double i = 0; i < COUNTER; i++)
+                {
+                    Math.Log(i);
+                }
+            });
+
+            GetResult("Log float", () =>
+            {
+                for (float i = 0; i < COUNTER; i++)
+                {
+                    Math.Log(i);
+                }
+            });
+
+            GetResult("Log int", () =>
+            {
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    Math.Log(i);
+                }
+            });
+
+            Console.WriteLine(new string('-', Console.WindowWidth));
+
+            GetResult("Sin int", () =>
+            {
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    Math.Sin(i);
+                }
+            });
+
+            GetResult("Sin float", () =>
+            {
+                for (int i = 0; i < COUNTER; i++)
+                {
+                    Math.Sin(i);
+                }
+            });
+
+            GetResult("Sin double", () =>
+            {
+                for (double i = 0; i < COUNTER; i++)
+                {
+                    Math.Sin(i);
+                }
+            });
+        }
+        public static void GetResult(string title, Action action)
+        {
+            Console.WriteLine(title);
+            st.Restart();
+            action();
+            st.Stop();
+            Console.WriteLine(st.Elapsed);
+            Console.WriteLine();
+        }
+    }
+}
