af1cdd80a005d9bf9ec947c28f4af33414fcef60
dbrezoev <d.brezoev@gmail.com>
2014-06-13 18:56:30 +0300

    jquery plugins first task only

---
+++ JavaScriptDOMUI/jQuery-Plugins/jQuery-Plugins/Scripts/01.DropDownMenuSelect.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    $.fn.dropdownSelect = function () {
+        var $this = $(this);
+        $this.hide();
+        var $div = $('<div>').addClass('dropdown-list-container');
+        var $ul = $('<ul>').addClass('dropdown-list-options');
+        var options = $('option');
+        var count = options.length;
+        for (var i = 0; i < count; i++) {
+            var $li = $('<li>').attr({ 'data-value': i + 1 });
+            $($li).addClass('dropdown-list-option');
+            $($li).text($(options[i]).text());
+            $($li).appendTo($ul);
+        }
+
+        $($ul).appendTo($div);
+        $div.appendTo(document.body);
+
+        var lis = $('li');
+        lis.each(function () {
+            var $this = $(this);
+            $this.on('click', function () {
+                $this.toggleClass('current');
+                var selector = 'option[value= ' + $this.attr('data-value') + ']';
+                if ($(selector).attr('selected') == 'selected') {
+                    $(selector).removeAttr('selected')
+                }
+                else {
+                    $(selector).attr('selected', 'selected');
+                }
+
+            })
+        })
+
+    }
+}());
---
+++ JavaScriptDOMUI/jQuery-Plugins/jQuery-Plugins/Scripts/01.DropdownMenu.js
@@ -0,0 +1,32 @@
+﻿/* Това е допълнителна задачка с която си експериментирах */
+
+(function () {        
+        $('#dropdown').css({ display: 'none' });
+        var divContainer = $('<div>').addClass('dropdown-list-container');
+        var chosen = $('<div id="chosen">').text($('option').first().text());
+        chosen.appendTo(divContainer);
+        var ul = $('<ul>').addClass('dropdown-list-options');
+        var optionsElements = $('option');
+        for (var i = 0; i < optionsElements.length; i++) {
+            var li = $('<li>');
+            li.addClass('dropdown-list-option');
+            li.attr('data-value', i);
+            var content = $(optionsElements[i]).text();
+            li.text(content);
+            li.appendTo(ul);
+        }
+        ul.appendTo(divContainer);
+        divContainer.appendTo(document.body);
+
+        $('li').each(function () {
+            $(this).on('click', function () {
+                chosen.text($(this).text());
+                $('ul').hide();
+            });
+        });
+
+        chosen.on('click', function () {
+            $('ul').toggle();
+        });
+    }
+}());
