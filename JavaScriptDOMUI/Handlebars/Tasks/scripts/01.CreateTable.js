71c17afe10b523934261d44929d911f6b5a6b7ca
dbrezoev <d.brezoev@gmail.com>
2014-06-14 18:17:40 +0300

    Handlebars tasks

---
+++ JavaScriptDOMUI/Handlebars/Tasks/scripts/01.CreateTable.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    var lectures = [{
+        number: 1,
+        title: 'Course Introduction',
+        date1: 'Wed 18:00, 28-May-2014',
+        date2: 'Thu 14:00, 29-May-2014',
+    }, {
+        number: 2,
+        title: 'Document Object Model',
+        date1: 'Wed 18:00, 28-May-2014',
+        date2: 'Thu 14:00, 29-May-2014',
+    }, {
+        number: 3,
+        title: 'HTML5 Canvas',
+        date1: 'Thu 18:00, 29-May-2014',
+        date2: 'Fri 14:00, 30-May-2014',
+    }, {
+        number: 4,
+        title: 'Kinetic JS Overview',
+        date1: 'Thu 18:00, 29-May-2014',
+        date2: 'Fri 14:00, 30-May-2014',
+    }, {
+        number: 5,
+        title: 'SVG and RaphaelJS',
+        date1: 'Wed 18:00, 04-Jun-2014',
+        date2: 'Thu 14:00, 05-Jun-2014',
+    }, ];      
+
+    var source = document.getElementById('source').innerHTML;
+    var template = Handlebars.compile(source);
+    var result = template({
+        lectures: lectures
+    });
+    document.getElementById('result').innerHTML = result;
+}());
---
+++ JavaScriptDOMUI/Handlebars/Tasks/scripts/02.DynamicSelect.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    var items = [{
+        value: 1,
+        text: 'One'
+    }, {
+        value: 2,
+        text: 'Two'
+    }, {
+        value: 3,
+        text: 'Three'
+    }, {
+        value: 4,
+        text: 'Four'
+    }, {
+        value: 5,
+        text: 'Five'
+    }];
+
+    var source = document.getElementById('source').innerHTML;
+    var template = Handlebars.compile(source);
+    document.getElementById('result').innerHTML = template({
+        items: items,
+    });
+}());
