aa2131f3ae6bc1043d6ab4a3cf3dc9b1eda2974a
Dobromir Brezoev <d.brezoev@gmail.com>
2014-06-25 22:21:58 +0300

    ClassicalOOP

---
+++ JS OOP/ClassicalOOP/DrawingModule/Scripts/Shapes.js
@@ -0,0 +1,30 @@
+﻿var drawingModule = (function () {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+    function drawCircle(centerX, centerY, radius) {
+        ctx.beginPath();        
+        ctx.arc(centerX, centerY, radius, 0, Math.PI * 2, false);
+        ctx.stroke();
+    }
+
+    function drawLine(x1, y1, x2, y2) {
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
+    }
+
+    function drawRect(x, y, width, height) {
+        ctx.moveTo(x, y);
+        ctx.strokeRect(x, y, width, height);
+    }
+
+    return {
+        draw :{
+            circle: drawCircle,
+            line: drawLine,
+            rect: drawRect,
+        }
+    }
+}());
+    
